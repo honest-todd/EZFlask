fca81ec462c52987896199d17f218a93e388b095
Jugegp16 <jugegp16@gmail.com>
2020-12-05 22:31:56 -0500

    update

--- script.py
+++ script.py
@@ -4,16 +4,45 @@ import csv
 import datetime
 from textblob import TextBlob
 from config import keys
+'''
 
-# authenicate Twitter dev credentials 
+    authorize Twitter API credentials for access
+    
+        You must:
+            1. apply for developer access
+            2. wait awhile :(
+            3. enter in your credentials to ./config.py dictionary
+
+
+'''
 auth = tw.OAuthHandler(keys["api_key"], keys["api_secret_key"])
 auth.set_access_token(keys["access_token"], keys["access_token_secret"])
 api = tw.API(auth)
 
-# retrive new data and add to data set 
-def update_data( keyword, filename):
 
-    # retrive old data
+def update_data( keyword, filename):
+'''
+
+    Get another set of tweets from Twitter using API access. 
+    Then caluclate new sentiment scores and update datset 
+    with the infromation.
+    
+    PARAMETERS
+    ----------5
+        keyword : str
+            the hashtag we are searching
+    
+        filename : str
+            filename corresponding to filepath containg data
+    
+    RETURNS
+    -------
+        void
+
+'''
+    # 
+    # retrieve current data from filpath.
+    # 
     fpath = os.path.join(keys['root_path'], 'data','{}.csv').format(filename)
     cur_data = csv.reader(open(fpath))
     for item in cur_data:
@@ -21,9 +50,13 @@ def update_data( keyword, filename):
          nue = item[1]
          neg = item[2]
     sentiment_scores = [int(pos),int(nue),int(neg)]
-
-    # update data
+    # 
+    # collect 200 new tweets
+    #
     tweets = tw.Cursor(api.search,q=keyword, lang="en",since=datetime.date.today()).items(200)
+    # 
+    # divide sentiments into categoires
+    #
     for tweet in tweets:
         t = TextBlob(str( tweet.text ))
         sentiment = t.sentiment.polarity
@@ -33,6 +66,9 @@ def update_data( keyword, filename):
             sentiment_scores[2]  += 1
         else:
             sentiment_scores[1]  += 1  
+    # 
+    # update file with updated data
+    #    
     writer = csv.writer(open(fpath,'w'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['positive' , 'nuetral', 'negative'])
     writer.writerow([str(sentiment_scores[0]) , str(sentiment_scores[1]), str(sentiment_scores[2])])
--- server.py
+++ server.py
@@ -6,13 +6,28 @@ from config import keys
 app = Flask(__name__)
 
 def extract_data(path1, path2):
-    data1 = csv.reader(open(path1))
-    data2 = csv.reader(open(path2))
-    for item in data1:
+    '''
+    Extract current data from the files - Iterate through files and 
+    compile both into a single list and converting datatypes into 
+    integers to be rendered to to the webpage.
+    
+    PARAMETERS
+    ----------
+        path1 : str
+            filepath of first dataset
+    
+        path2 : str
+            filepath of second dataset
+    
+    RETURNS
+    -------
+        list : single list of sentiment scores
+    '''
+    for item in csv.reader(open(path1)):
         pos1 = item[0]
         nue1 = item[1]
         neg1 = item[2] 
-    for item in data2:
+    for item in csv.reader(open(path2)):
         pos2 = item[0]
         nue2 = item[1]
         neg2 = item[2]
@@ -20,10 +35,21 @@ def extract_data(path1, path2):
 
 @app.route("/")
 def home():
+    '''
+    
+    This is the routing for the home page of the website(only page).
+    Extract current data and render the HTML template. 
+    
+
+    RETURNS
+    -------
+        rendered HTML template containing home screen.
+        
+
+    '''
     data = extract_data( os.path.join(app.root_path, 'data', 'data1.csv'), os.path.join(app.root_path, 'data', 'data2.csv') )
-    titles = [ keys['input1'], keys['input2'] ]
-    return render_template('index.html', data = data, titles = titles)
+    return render_template('index.html', data = data, titles = [ keys['input1'], keys['input2'] ])
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
+    app.run(host='127.0.0.1', port=os.environ.get('PORT', 5500), debug=True)
 
